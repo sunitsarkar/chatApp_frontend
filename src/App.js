a01ac20e7189578c6e35ee7a8c14487301d2036b
sunit sarkar <sunit6503@gmail.com>
2024-01-20 21:33:05 +0530

    private room feature

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import './App.css';
 import io from 'socket.io-client'
 import {useState,useEffect} from 'react'
 
+// const socket=io.connect("http://localhost:8000");
 const socket=io.connect("https://chatapp-backend-qmqc.onrender.com");
+
 
 function App() {
 
   const [message,setMessage]=useState('');
-  const [received,setReceived]=useState('')
+  const [received,setReceived]=useState('');
+  const [room,setRoom]=useState('')
+
+  const joinRoom=()=>{
+    if(room !== ""){
+      socket.emit("join_room", room)
+    }
+  }
 
   const sendMessage=()=>{
     message?socket.emit('message',{
-      message:message
+      message:message,
+      room:room
     }):alert('cannot send empty message')
     setMessage('')
   }
@@ -24,9 +34,11 @@ function App() {
 
   return (
     <div className="App">
+      <input placeholder='type room no to join' value={room}  style={{height:'30px',width:'250px'}} onChange={(e)=>{setRoom(e.target.value)}}/>
+      <button style={{height:'30px',width:'150px'}} onClick={joinRoom}>join private room</button>
       <input placeholder='type message' value={message}  style={{height:'30px',width:'250px'}} onChange={(e)=>{setMessage(e.target.value)}}/>
       <button style={{height:'30px',width:'150px'}} onClick={sendMessage}>send message</button>
-      <h1>Messages</h1>
+      <h3>Messages of room {room}</h3>
       {
         received?<li>{received}</li>:null
       }
